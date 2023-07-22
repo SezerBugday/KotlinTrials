9d06ed88355ba6d0e3aa762eeb2ead59607dde3e
Sezer Buğday <sezerbugday2@gmail.com>
2023-07-22 17:28:44 +0300

    created project

---
+++ TrialOne_SuperHero/app/src/main/java/com/sezer/trialone_superhero/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.sezer.trialone_superhero
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sezer.trialone_superhero.ui.theme.TrialOne_SuperHeroTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TrialOne_SuperHeroTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TrialOne_SuperHeroTheme {
+        Greeting("Android")
+    }
+}
